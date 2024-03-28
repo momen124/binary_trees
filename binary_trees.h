90ebd1775eceb66f794a77914909f8bdd9756cef
MuhabGamal <muhabme@gmail.com>
2024-03-29 01:06:04 +0200

    binary_tree_is_avl

--- binary_trees.h
+++ binary_trees.h
@@ -63,7 +63,7 @@ binary_tree_t *binary_tree_sibling(binary_tree_t *node);
 binary_tree_t *binary_tree_uncle(binary_tree_t *node);
 /*  ------------------ advanced task -----------------------------------*/
 binary_tree_t *binary_trees_ancestor(const binary_tree_t *first,
-const binary_tree_t *second);
+									 const binary_tree_t *second);
 void binary_tree_levelorder(const binary_tree_t *tree, void (*func)(int));
 int binary_tree_is_complete(const binary_tree_t *tree);
 binary_tree_t *binary_tree_rotate_left(binary_tree_t *tree);
@@ -83,5 +83,7 @@ heap_t *heap_insert(heap_t **root, int value);
 heap_t *array_to_heap(int *array, size_t size);
 int heap_extract(heap_t **root);
 int *heap_to_sorted_array(heap_t *heap, size_t *size);
+size_t binary_tree_height(const binary_tree_t *tree);
+int binary_tree_is_avl_helper(const binary_tree_t *tree, int low, int hi);
 
 #endif /* _BINARY_TREES_H_ */
